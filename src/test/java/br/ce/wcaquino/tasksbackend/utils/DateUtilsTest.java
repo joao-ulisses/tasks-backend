9b252ad15aeb13d86db449ef48e06e712ab6e671
João Ulisses <sirnobin@gmail.com>
2024-11-06 00:12:30 -0300

    rollback

--- src/test/java/br/ce/wcaquino/tasksbackend/utils/DateUtilsTest.java
+++ src/test/java/br/ce/wcaquino/tasksbackend/utils/DateUtilsTest.java
@@ -18,7 +18,7 @@ public class DateUtilsTest {
 	@Test
 	public void deveRetornarFalseParaDatasPassadas() {
 		LocalDate date = LocalDate.of(2010, 01, 01);
-		Assert.assertTrue(DateUtils.isEqualOrFutureDate(date));
+		Assert.assertFalse(DateUtils.isEqualOrFutureDate(date));
 	}
 	
 	@Test
